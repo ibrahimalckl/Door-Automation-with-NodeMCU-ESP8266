1db0f5884d9ad980e082dbd14ac33056f09119bb
İbrahim Alaçakal <ibrahim.alacakal@outlook.com>
2022-10-17 22:51:16 +0300

    Update nodemcu.ino

--- nodemcu/nodemcu.ino
+++ nodemcu/nodemcu.ino
@@ -1,29 +1,23 @@
+#include "FirebaseESP8266.h"
 #include <ESP8266WiFi.h>
-#include <ESP8266WebServer.h>
 #include <WiFiManager.h>
 
-/*Ağ adınızı ve Şifrenizi Giriniz*/
-//const char* ssid = "***********";  // Ağ adınızı girin
-//const char* password = "************";  //Ağ şifrenizi girin
+//1. Firebase veritabanı adresini, Token bilgisini ve ağ adresi bilgilerinizi giriniz.
+#define FIREBASE_HOST "" // http:// veya https:// olmadan yazın
+#define FIREBASE_AUTH ""
 
-ESP8266WebServer server(80); //80 portunu kullanarak bir webserver nesnesi oluşturduk
 WiFiManager wm;
-
-uint8_t LED1pin = D7;
-bool LED1status = LOW;
-
-uint8_t LED2pin = D6;
-bool LED2status = LOW;
 
 bool res;
 
+//2. veritabanim adında bir firebase veritabanı nesnesi oluşturuyoruz
+FirebaseData veritabanim;
+#define firebaseControl "{\"led\":\"1\"}"
 
-void setup() {
+void setup()
+{
+
   Serial.begin(115200);
-  delay(100);
-  pinMode(LED1pin, OUTPUT);
-  pinMode(LED2pin, OUTPUT);
-
 
   //////////////WIFI MANAGER AYAR BLOĞU//////////////////////////////////////////////////
 
@@ -45,109 +39,77 @@ void setup() {
 
 /////////////////////////////////////////////////////////////////////////////////////////
   
-  //Serial.println(ssid);
-  //Serial.println(" Ağına Bağlanılıyor");
-
-  //yerel ağınıza bağlanmanız için gerekli komut
-  //WiFi.begin(ssid, password);
-
-  //bağlantının gerçekleştiğini kontrol ediyoruz
-  while (WiFi.status() != WL_CONNECTED) {
-  delay(1000);
-  Serial.print("."); //gerçekleşene kadar loading gibi işaret yaptırıyoruz
+  while (WiFi.status() != WL_CONNECTED)
+  {
+    Serial.print(".");
+    delay(300);
   }
-  Serial.println("");
-  Serial.println("Ağ Bağlantısı Sağlandı..!");
-  Serial.print("IP Adresiniz: ");  Serial.println(WiFi.localIP());
+  Serial.println();
+  Serial.print("IP adresine bağlanıldı: ");
+  Serial.println(WiFi.localIP());
+  Serial.println();
 
-  server.on("/", handle_OnConnect);
-  server.on("/led1on", handle_led1on);
-  server.on("/led1off", handle_led1off);
-  server.on("/led2on", handle_led2on);
-  server.on("/led2off", handle_led2off);
-  server.onNotFound(handle_NotFound);
 
-  server.begin();
-  Serial.println("HTTP Sunucusu Başlatıldı");
+  //3. Firebase bağlantısı başlatılıyor
+
+  Firebase.begin(FIREBASE_HOST, FIREBASE_AUTH);
+
+  //4. Ağ bağlantısı kesilirse tekrar bağlanmasına izin veriyoruz
+  Firebase.reconnectWiFi(true);
+ pinMode(D2,OUTPUT);
+ digitalWrite(D2,LOW);
 }
-void loop() {
-  server.handleClient();
-  if(LED1status)
-  {digitalWrite(LED1pin, HIGH);}
-  else
-  {digitalWrite(LED1pin, LOW);}
+
+void loop()
+{
+
+  if(Firebase.getString(veritabanim, "/led")) //Alınacak veri tipine göre getInt, getBool, getFloat, getDouble, getString olarak kullanılabilir.
+  {
+    //bağlantı başarılı ve veri geliyor ise
+    Serial.print("String tipinde veri alımı başarılı, veri = ");
+    Serial.println(veritabanim.stringData());
+    //firebaseControl = veritabanim.stringData();
+    
+    //cout << "1.index:" << sifre[1] << endl;
+    
+    if (veritabanim.stringData()==firebaseControl){
+      digitalWrite(D2,HIGH);
+      Serial.print("Led Yak");
+      
+    }
+    else {
+      
+      digitalWrite(D2,LOW);
+      Serial.print("Led Sondur");
+      Serial.print(firebaseControl);
+      }
+    
+    
+
+  }else{
+    //hata varsa hata mesajı ve nedeni yazdırılıyor
+
+    Serial.print("Str verisi çekilemedi, ");
+    Serial.println(veritabanim.errorReason());
+  }
+ 
+
+
   
-  if(LED2status)
-  {digitalWrite(LED2pin, HIGH);}
-  else
-  {digitalWrite(LED2pin, LOW);}
-}
+////////////////////////////////////////////////////////////////////////////////////
+// firebase veritabanına veri göndermek için Firebase.setInt komutu kullanılabilir.
+//if(Firebase.setInt(veritabanim, "/led", a))
+//  {
+//    //bağlantı başarılı ve veri geliyor ise
+//     Serial.println("Int tipinde veri gönderimi başarılı");
+// 
+//  }else{
+//    //hata varsa hata mesajı ve nedeni yazdırılıyor
+//
+//    Serial.print("Int tipindeki veri gönderilemedi, ");
+//    Serial.println(veritabanim.errorReason());
+//  }
 
-void handle_OnConnect() {
-  LED1status = LOW;
-  LED2status = LOW;
-  Serial.println("GPIO7 Durumu: OFF | GPIO6 Durumu: OFF");
-  server.send(200, "text/html", SendHTML(LED1status,LED2status)); 
-}
 
-void handle_led1on() {
-  LED1status = HIGH;
-  Serial.println("GPIO7 Durumu: ON");
-  server.send(200, "text/html", SendHTML(true,LED2status)); 
-}
-
-void handle_led1off() {
-  LED1status = LOW;
-  Serial.println("GPIO7 Durumu: OFF");
-  server.send(200, "text/html", SendHTML(false,LED2status)); 
-}
-
-void handle_led2on() {
-  LED2status = HIGH;
-  Serial.println("GPIO6 Durumu: ON");
-  server.send(200, "text/html", SendHTML(LED1status,true)); 
-}
-
-void handle_led2off() {
-  LED2status = LOW;
-  Serial.println("GPIO6 Durumu: OFF");
-  server.send(200, "text/html", SendHTML(LED1status,false)); 
-}
-
-void handle_NotFound(){
-  server.send(404, "text/plain", "Sayfa Bulunamadı");
-}
-
-String SendHTML(uint8_t led1stat,uint8_t led2stat){
-  String ptr = "<!DOCTYPE html> <html>\n";
-  ptr +="<head><meta name=\"viewport\" content=\"width=device-width, initial-scale=1.0, user-scalable=no\">\n";
-  ptr +="<meta http-equiv=\"Content-Type\" content=\"text/html;charset=UTF-8\">";
-  ptr +="<title>LED ve Röle Kontrol</title>\n";
-  ptr +="<style>html { font-family: Helvetica; display: inline-block; margin: 0px auto; text-align: center;}\n";
-  ptr +="body{margin-top: 50px;} h1 {color: #444444;margin: 50px auto 30px;} h3 {color: #444444;margin-bottom: 50px;}\n";
-  ptr +=".button {display: block;width: 80px;background-color: #1abc9c;border: none;color: white;padding: 13px 30px;text-decoration: none;font-size: 25px;margin: 0px auto 35px;cursor: pointer;border-radius: 4px;}\n";
-  ptr +=".button-on {background-color: #1abc9c;}\n";
-  ptr +=".button-on:active {background-color: #16a085;}\n";
-  ptr +=".button-off {background-color: #34495e;}\n";
-  ptr +=".button-off:active {background-color: #2c3e50;}\n";
-  ptr +="p {font-size: 14px;color: #888;margin-bottom: 10px;}\n";
-  ptr +="</style>\n";
-  ptr +="</head>\n";
-  ptr +="<body>\n";
-  ptr +="<h1>ESP8266 Web Server Uygulaması</h1>\n";
-    ptr +="<h3>ESP Station(STA) Modunda Kullanılıyor</h3>\n";
   
-   if(led1stat)
-  {ptr +="<p>LED1 Durum: AÇIK</p><a class=\"button button-off\" href=\"/led1off\">KAPAT</a>\n";}
-  else
-  {ptr +="<p>LED1 Durum: KAPALI</p><a class=\"button button-on\" href=\"/led1on\">AÇ</a>\n";}
-
-  if(led2stat)
-  {ptr +="<p>LED2 Durum: AÇIK</p><a class=\"button button-off\" href=\"/led2off\">KAPAT</a>\n";}
-  else
-  {ptr +="<p>LED2 Durum: KAPALI</p><a class=\"button button-on\" href=\"/led2on\">AÇ</a>\n";}
-
-  ptr +="</body>\n";
-  ptr +="</html>\n";
-  return ptr;
 }
